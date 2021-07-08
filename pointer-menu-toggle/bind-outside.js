0f1f8a3664136167fa521f700a2a4798e90b3453
dotdesign23 <61997056+dotdesign23@users.noreply.github.com>
2021-07-08 23:57:16 +0700

    Update bind-outside.js

--- pointer-menu-toggle/bind-outside.js
+++ pointer-menu-toggle/bind-outside.js
@@ -1,5 +1,7 @@
+const activePath = "/chart/apex-chart";
+
 document.querySelectorAll('.menu-item-link.active:not(.menu-item-toggle)').forEach( element => {
   element.classList.remove("active");
 });
 
-document.querySelector('.menu-item-link[href="/chart/apex-chart"]').classList.add("active");
+document.querySelector(`.menu-item-link[href="${activePath}"]`).classList.add("active");
